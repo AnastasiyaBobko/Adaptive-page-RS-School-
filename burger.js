ec56cf38598a6a278ce8c4aefa247b729e5d9453
AnastasiyaBobko <a.s.bobko@gmail.com>
2023-02-12 20:48:17 +0300

    init

---
+++ burger.js
@@ -0,0 +1,18 @@
+let burger  = document.querySelector(".burger");
+let navMenu = document.querySelector(".navigation");
+
+burger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+    burger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
+
+const navLink = document.querySelectorAll(".nav-link");
+
+navLink.forEach(n => n.addEventListener("click", closeMenu));
+
+function closeMenu() {
+    burger.classList.remove("active");
+    navMenu.classList.remove("active");
+}
